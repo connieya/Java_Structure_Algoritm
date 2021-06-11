56fc0b20334d839538ac3394cfded39c1cc4611e
gunny6026 <gunny6026@naver.com>
2021-06-11 22:58:17 +0900

    합병 정렬

--- src/com/company/study/algorithm/lesson02_sort/chap02/MergeSort.java
+++ src/com/company/study/algorithm/lesson02_sort/chap02/MergeSort.java
@@ -1,4 +1,29 @@
 package com.company.study.algorithm.lesson02_sort.chap02;
 
 public class MergeSort {
+
+    static void merge(int data[], int p , int q , int r){
+        int i =p , j = q +1 , k = p;
+        int tmp[data.length];
+        while (i<=q && j<=r){
+            if (data[i] <= data[j]){
+                tmp[k++] = data[i++];
+            }else {
+                tmp[k++] = data[j++];
+            }
+        }
+        while (i<=q){
+            tmp[k++] = data[i++];
+        }
+        while (j<=r){
+            tmp[k++] = data[j++];
+        }
+        for (int m = p; m<=r; i++){
+            data[m] = tmp[m];
+        }
+    }
+    public static void main(String[] args) {
+        int data[] = {1,2,10,3,4,9,19,30,21,43,56,2};
+
+    }
 }
